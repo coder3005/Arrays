86dbad5c1899c0d6ce738018a2a6f35252a6d007
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2024-01-26 00:08:12 +0530

    Create insertionsort.java

---
+++ insertionsort.java
@@ -0,0 +1,28 @@
+package Arrays;
+
+public class insertionsort {
+    public static void Insertionsort(int arr[]) {
+        for(int i=1;i<arr.length;i++) {
+            int curr=arr[i];
+            int prev=i-1;
+            //finding out the correct pos to insert
+            while(prev>=0 && arr[prev]>curr) {
+                arr[prev+1]=arr[prev];
+                prev--;
+            }
+            //insertion
+            arr[prev+1]=curr;
+        }
+    }
+    public static void printArr(int arr[]) {
+        for(int i=0;i<arr.length;i++) {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int arr[]={5,4,1,3,2};
+        Insertionsort(arr);
+        printArr(arr);
+    }
+}
