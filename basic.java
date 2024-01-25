e793cde6adc46195de991c58b998189736e395f5
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2024-01-26 00:06:49 +0530

    Create basic.java

---
+++ basic.java
@@ -0,0 +1,12 @@
+package Arrays;
+
+public class basic {
+    public static void main(String[] args) {
+        int marks[]=new int[50];
+
+        int numbers[]={1,2,3}; //3
+
+        String fruits[]={"apple", "mango", "orange"};
+        
+    }
+}
